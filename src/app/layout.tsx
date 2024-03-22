093b230b257e9658c27bc3a1fcb73f31b5dce73b
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-03-22 14:28:55 +0100

    feat: meta image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./global.css";
 export const metadata: Metadata = {
   title: "Mariage Anouck & Benjamin",
   description: "Toutes les infos sur le mariage :)",
+  openGraph: {
+    images: ["/header.jpg"],
+  },
 };
 
 export default function RootLayout({
