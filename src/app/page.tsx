8317b4849a0dbc46f8b61bb9b6dcae9a6e4c6e77
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-08-27 18:56:29 +0200

    feat: remove password

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,9 @@
-import styles from "./page.module.css";
-import { cookies } from "next/headers";
-import Form from "@/components/Form";
-import bcrypt from "bcrypt";
 import MainPage from "@/components/MainPage";
-import SecondPage from "@/components/SecondPage";
 
 export default function Home() {
-  const cookiesStore = cookies();
-  const loginCookies = cookiesStore.get(process.env.PASSWORD_COOKIE_NAME!);
-  const mainPage =
-    loginCookies &&
-    bcrypt.compareSync(process.env.MAIN_PAGE_PASSWORD!, loginCookies.value);
-
-  if (mainPage) {
-    return (
-      <main>
-        <MainPage />
-      </main>
-    );
-  }
-
-  const secondPage =
-    loginCookies &&
-    bcrypt.compareSync(process.env.SECOND_PAGE_PASSWORD!, loginCookies.value);
-  if (secondPage) {
-    return (
-      <main>
-        <SecondPage />
-      </main>
-    );
-  }
   return (
     <main>
-      <Form />
+      <MainPage />
     </main>
   );
 }
